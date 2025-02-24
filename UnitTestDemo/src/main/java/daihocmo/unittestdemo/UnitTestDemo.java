7c1e526882aca286c7626a7357390c199d5c1bd6
HoangYen04 <nguyenhoangyensao@gmail.com>
2025-02-24 14:13:54 +0700

    update feature-test

--- UnitTestDemo/src/main/java/daihocmo/unittestdemo/UnitTestDemo.java
+++ UnitTestDemo/src/main/java/daihocmo/unittestdemo/UnitTestDemo.java
@@ -12,6 +12,6 @@ public class UnitTestDemo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello Yen!");
+        System.out.println("Hello 02 Yen!");
     }
 }
